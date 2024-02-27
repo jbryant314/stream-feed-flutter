0cdbb39831925bafaad58a05dbe0ff5e505bb347
marin <mvirlogeux@twipi-group.com>
2024-02-27 15:19:47 +0100

    fix: missing export

--- packages/stream_feed/lib/src/client/index.dart
+++ packages/stream_feed/lib/src/client/index.dart
@@ -3,6 +3,7 @@ export 'analytics_client.dart';
 export 'file_storage_client.dart';
 export 'flat_feed.dart';
 export 'image_storage_client.dart';
+export 'notification_feed.dart';
 export 'reactions_client.dart';
 export 'stream_feed_client.dart';
 export 'stream_user.dart';
